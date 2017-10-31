2c4b0287f33c0ce4b53f60040309ad09c98520fa
karsteneb <karsten@eberding.eu>
2017-10-31 11:02:23 +0100

    Update

--- en.dict.dcmdb.php
+++ en.dict.dcmdb.php
@@ -44,14 +44,50 @@ Dict::Add('EN US', 'English', 'English', array(
     'Class:DocGroup/Attribute:docimplementation_list' => 'Implementations',
 
     'Class:DocArtifact' => 'Document Artifact',
-    'Class:DocArtifact/Name' => '%1$s',
+    'Class:DocArtifact/Name' => '%1$s/%2$s',
     'Class:DocArtifact/Attribute:name' => 'Name',
     'Class:DocArtifact/Attribute:subgroup' => 'Subgroup',
     'Class:DocArtifact/Attribute:region' => 'Region',
+    'Class:DocArtifact/Attribute:lifecyclecat' => 'LC Category',
+    'Class:DocArtifact/Attribute:autonaming' => 'Auto Naming',
+    'Class:DocArtifact/Attribute:folderpath' => 'Folder Path',
     'Class:DocArtifact/Attribute:docprocess_id' => 'Document Process',
     'Class:DocArtifact/Attribute:doccategory_id' => 'Document Category',
     'Class:DocArtifact/Attribute:docgroup_id' => 'Document Group',
+    'Class:DocArtifact/Attribute:docsolution_id' => 'Document Solution',
+    'Class:DocArtifact/Attribute:docobjecttype_id' => 'Document Object Type',
     'Class:DocArtifact/Attribute:description' => 'Description',
+    'Class:DocArtifact/Attribute:docpropertypage_list' => 'Document Property Pages',
+
+    'Class:DocProperty' => 'Document Property',
+    'Class:DocProperty/Name' => '%1$s',
+    'Class:DocProperty/Attribute:name' => 'Name',
+    'Class:DocProperty/Attribute:label' => 'Label',
+    'Class:DocProperty/Attribute:datatype' => 'Data Type',
+    'Class:DocProperty/Attribute:fieldtype' => 'Field Type',
+    'Class:DocProperty/Attribute:repeating' => 'Repeating',
+    'Class:DocProperty/Attribute:required' => 'Required',
+    'Class:DocProperty/Attribute:docobjecttype_id' => 'Document Object Type',
+    'Class:DocProperty/Attribute:docdictionary_id' => 'Document Dictionary',
+    'Class:DocProperty/Attribute:description' => 'Description',
+    'Class:DocProperty/Attribute:docpropertypage_list' => 'Document Property Pages',
+
+    'Class:DocPropertyPage' => 'Document Property Page',
+    'Class:DocPropertyPage/Name' => '%1$s',
+    'Class:DocPropertyPage/Attribute:name' => 'Name',
+    'Class:DocPropertyPage/Attribute:description' => 'Description',
+    'Class:DocPropertyPage/Attribute:docproperty_list' => 'Document Properties',
+    'Class:DocPropertyPage/Attribute:docartifact_list' => 'Document Artifacts',
+
+    'Class:DocObjectType' => 'Document Object Type',
+    'Class:DocObjectType/Attribute:name' => 'Name',
+    'Class:DocObjectType/Attribute:description' => 'Description',
+    'Class:DocObjectType/Attribute:docproperty_list' => 'Document Properties',
+
+    'Class:DocDictionary' => 'Document Dictionary',
+    'Class:DocDictionary/Attribute:name' => 'Name',
+    'Class:DocDictionary/Attribute:description' => 'Description',
+    'Class:DocDictionary/Attribute:docproperty_list' => 'Document Properties',
 
     'Class:DocSolution' => 'DM Solution',
     'Class:DocSolution/Attribute:docimplementation_list' => 'DM Implementations',
@@ -70,6 +106,13 @@ Dict::Add('EN US', 'English', 'English', array(
     'Class:lnkDocImplementationToDocGroup/Attribute:docimplementation_id' => 'DM Implementation',
     'Class:lnkDocImplementationToDocGroup/Attribute:docgroup_id' => 'Document Group',
     
+    'Class:lnkDocPropertyPageToDocProperty' => 'Property Page to Property',
+    'Class:lnkDocPropertyPageToDocProperty/Attribute:docpropertypage_id' => 'Document Property Page',
+    'Class:lnkDocPropertyPageToDocProperty/Attribute:docproperty_id' => 'Document Property',
+    
+    'Class:lnkDocPropertyPageToDocArtifact' => 'Property Page to Artifact',
+    'Class:lnkDocPropertyPageToDocArtifact/Attribute:docpropertypage_id' => 'Document Property Page',
+    'Class:lnkDocPropertyPageToDocArtifact/Attribute:docartifact_id' => 'Document Artifact',
 
     //Menu and Dashboard
     'Menu:DocTypes' => 'Document Management Applications',
@@ -85,6 +128,10 @@ Dict::Add('EN US', 'English', 'English', array(
     'Menu:DocTypes:DocCategory' => 'Document Categories',
     'Menu:DocTypes:DocGroup' => 'Document Groups',
     'Menu:DocTypes:DocArtifact' => 'Document Artifacts',
+    'Menu:DocTypes:DocProperty' => 'Document Properties',
+    'Menu:DocTypes:DocPropertyPage' => 'Document Property Pages',
+    'Menu:DocTypes:DocObjectType' => 'Document Object Types',
+    'Menu:DocTypes:DocDictionary' => 'Document Dictionaries',
     
 ));
   
